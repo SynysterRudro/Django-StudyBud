105d9c15dd20436535c9b6e6b11b5aaa8fc6fa18
Rudro <user@Rudro-Avenger.local>
2025-11-18 19:01:35 +0600

    login method added succressfully

--- base/views.py
+++ base/views.py
@@ -7,9 +7,34 @@ from .forms import RoomForm
 # Create your views here.
 
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 #  
+ 
+
+def loginPage(request):
+     if request.method == 'POST':
+          username = request.POST.get('username') 
+          password = request.POST.get('password')
+
+          try:
+               user = User.objects.get(username=username)
+          except:
+               # print("User does not exist") 
+               messages.error(request, 'User does not exist')
+          user = authenticate(request, username=username, password=password)
+          if user is not None:
+               login(request, user)
+               return redirect('home')
+          else:
+               messages.error(request, 'Username OR password does not exist')
+
+     context = {}
+     return render(request, 'base/login_register.html',context)
+
 
 def home(request):
      # return HttpResponse("Home Page")
@@ -22,7 +47,7 @@ def home(request):
                                   )
      topics = Topic.objects.all()
      room_count = rooms.count()
-     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count }
      return render(request, 'base/home.html', context)
 
 def room(request,pk):
