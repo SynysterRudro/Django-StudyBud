7a0151feedc873be756b25aab17e38c5599dbe6e
Rudro <user@Rudro-Avenger.local>
2025-11-23 22:01:05 +0600

    profile page created

--- base/urls.py
+++ base/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>', views.update_Room, name='update-room'),
     path('delete-room/<str:pk>', views.delete_Room, name='delete-room'),
-    path('delete-message/<str:pk>', views.delete_Message, name='delete-message')
+    path('delete-message/<str:pk>', views.delete_Message, name='delete-message'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile')
     
 ] 
--- base/views.py
+++ base/views.py
@@ -106,6 +106,14 @@ def room(request,pk):
 
      return render(request, 'base/room.html',context)
 
+def userProfile(request, pk):
+     user = User.objects.get(id=pk)
+     rooms = user.room_set.all()
+     room_messages = user.message_set.all()
+     topics = Topic.objects.all()
+     context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+     return render(request, 'base/profile.html', context) 
+
 
 # the line says only logged in users can access this view
 @login_required(login_url='login')
