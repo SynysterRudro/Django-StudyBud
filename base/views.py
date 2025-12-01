9c68d4d119292d138106ae25d696bd157b134b09
Rudro <user@Rudro-Avenger.local>
2025-12-01 19:27:40 +0600

    add room functionality revised

--- base/views.py
+++ base/views.py
@@ -118,21 +118,32 @@ def userProfile(request, pk):
 # the line says only logged in users can access this view
 @login_required(login_url='login')
 def createRoom(request):
+     topics = Topic.objects.all() 
      form = RoomForm()
      if request.method == 'POST':
-          form = RoomForm(request.POST)
-          if form.is_valid():
-               room = form.save(commit=False)
-               room.host = request.user
-               room.save()
-               return redirect('home')
+          topic_name = request.POST.get('topic')
+          topic, created = Topic.objects.get_or_create(name=topic_name)
+          Room.objects.create(
+               host=request.user,
+               topic = topic,
+               name = request.POST.get('name'),
+               description = request.POST.get('description')
+               )
+          # form = RoomForm(request.POST)  
 
-     context ={'form':form}
+          # if form.is_valid():
+          #      room = form.save(commit=False)
+          #      room.host = request.user
+          #      room.save()
+          return redirect('home')
+
+     context ={'form':form, 'topics': topics}
      return render(request,'base/room_form.html',context) 
 
 @login_required(login_url='login')
 def update_Room(request, pk):
     room = Room.objects.get(id=pk)  # Define 'room' first
+    topics = Topic.objects.all()
 
     form = RoomForm(instance=room)
 
@@ -145,7 +156,7 @@ def update_Room(request, pk):
             form.save()
             return redirect('home')
 
-    context = {'form': form} 
+    context = {'form': form, 'topics': topics} 
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
